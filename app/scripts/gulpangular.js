130c3d4d9cfcb0002c2b5780d8b4b4c566eb553d
Andrew Vorobyov <vorandrew@gmail.com>
2014-07-12 14:34:54 +0300

    gulp-angular

---
+++ app/scripts/gulpangular.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('gulpangular', ['ngCookies', 'ui.router'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'partials/main.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  });
