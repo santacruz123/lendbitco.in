032a8cd2e9f28a687d88cd5290b8c45158b2e7ef
Andrew Vorobyov <vorandrew@gmail.com>
2014-08-08 15:27:54 +0300

    Account change

--- app/scripts/account/account-lb-account-directive.js
+++ app/scripts/account/account-lb-account-directive.js
@@ -1,15 +1,19 @@
 'use strict';
 
 angular.module('lendbitcoin')
-  .directive('account', function (Account, Ripple) {
+  .directive('account', function (Account, Ripple, Platform, Orders) {
     return {
       templateUrl: 'partials/account/account-lb-account-directive.html',
       restrict: 'E',
       scope: {},
       controller: function ($scope) {
         $scope.$watch('user.acc', function (n) {
-          Account.acc = n;
-          $scope.isSecretSet = false;
+          if (n) {
+            Account.acc = n;
+            $scope.isSecretSet = false;
+            Platform.updateBalances(Platform.reloadCb);
+            Orders.updateOrders(Platform.reloadCb);
+          }
         });
 
         $scope.setSecret = function (secret) {
